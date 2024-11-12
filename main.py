f0d3183f4e7629bb9ce64092f349f724cdb3f430
Satyam Seth <satyam1998.1998@gmail.com>
2024-11-13 01:08:56 +0530

    add .env support

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 app = FastAPI()
+
+class Settings(BaseSettings):
+    """App Settings"""
+
+    allowed_hostname: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings=Settings()
 
 @app.get("/")
 async def health_check():
     """Health Check"""
 
-    return {"status": "healthy"}
+    return {"status": "healthy", "allowed_hostname": settings.allowed_hostname}
